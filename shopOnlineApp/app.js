df26bbc953e86a297a0dda3fd6dd1a0373f048eb
vanbumi <jsp.dio@gmaile.com>
2018-05-03 08:17:41 +0700

    part 2 connect to mongodb

--- shopOnlineApp/app.js
+++ shopOnlineApp/app.js
@@ -1,5 +1,17 @@
 var express = require('express');
 var path = require('path');
+var mongoose = require('mongoose');
+var config = require('./config/database');
+
+mongoose.connect(config.database);
+
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+  console.log('Conected to database')
+});
 
 // Initial
 var app = express();
