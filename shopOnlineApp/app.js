b86d7f84ca602dcb08c7fefbd96e9b991c485291
vanbumi <jsp.dio@gmaile.com>
2018-10-26 13:53:54 +0700

    materi_19 done w/ no problem at all

--- shopOnlineApp/app.js
+++ shopOnlineApp/app.js
@@ -1,33 +1,37 @@
-var express = require('express');
-var path = require('path');
-var mongoose = require('mongoose');
-var config = require('./config/database');
-var bodyParser = require('body-parser');
-var session = require('express-session');
-var expressValidator = require('express-validator');
+var express = require("express");
+var path = require("path");
+var mongoose = require("mongoose");
+var config = require("./config/database");
+var bodyParser = require("body-parser");
+var session = require("express-session");
+var expressValidator = require("express-validator");
+var fileUpload = require("express-fileupload");
 
 // Connect to db
 mongoose.connect(config.database);
 var db = mongoose.connection;
 
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function() {
   // we're connected!
-  console.log('Conected to database')
+  console.log("Conected to database");
 });
 
 // Initial
 var app = express();
 
 // View engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 // Set public folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Setup Global errors variable
 app.locals.errors = null;
+
+// Express fileUpload middleware
+app.use(fileUpload());
 
 // Setup body parser middleware
 // parse application/x-www-form-urlencoded
@@ -36,58 +40,55 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Setup express session middleware
-app.use(session({
-  secret: 'keyboard cat',
-  resave: true,
-  saveUninitialized: true,
-  //cookie: { secure: true }
-}))
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: true,
+    saveUninitialized: true
+    //cookie: { secure: true }
+  })
+);
 
 // Setup express validator middleware
-app.use(expressValidator({
-  errorFormatter: function (param, msg, value) {
-    var namespace = param.split('.')
-            , root = namespace.shift()
-            , formParam = root;
+app.use(
+  expressValidator({
+    errorFormatter: function(param, msg, value) {
+      var namespace = param.split("."),
+        root = namespace.shift(),
+        formParam = root;
 
-    while (namespace.length) {
-        formParam += '[' + namespace.shift() + ']';
-    }
-    return {
+      while (namespace.length) {
+        formParam += "[" + namespace.shift() + "]";
+      }
+      return {
         param: formParam,
         msg: msg,
         value: value
-    };
-  }
-}));
+      };
+    }
+  })
+);
 
 // Setup express messages middleware
-app.use(require('connect-flash')());
-app.use(function (req, res, next) {
-  res.locals.messages = require('express-messages')(req, res);
+app.use(require("connect-flash")());
+app.use(function(req, res, next) {
+  res.locals.messages = require("express-messages")(req, res);
   next();
 });
 
-
-
-
-
-// set routes 
-var pages = require('./routes/pages.js');
-var adminPages = require('./routes/admin_pages.js');
-var adminCategories = require('./routes/admin_categories.js');
+// set routes
+var pages = require("./routes/pages.js");
+var adminPages = require("./routes/admin_pages.js");
+var adminCategories = require("./routes/admin_categories.js");
+var adminProducts = require("./routes/admin_products.js");
 
 // setup links
-app.use('/admin/pages', adminPages);
-app.use('/admin/categories', adminCategories);
-app.use('/', pages);
-
-
-
-
+app.use("/admin/pages", adminPages);
+app.use("/admin/categories", adminCategories);
+app.use("/", pages);
 
 // Setup server
 var port = 3000;
-app.listen(port, function(){
-  console.log("Server running on port " + port)
+app.listen(port, function() {
+  console.log("Server running on port " + port);
 });
---
+++ shopOnlineApp/routes/admin_products.js
@@ -0,0 +1,185 @@
+var express = require("express");
+var router = express.Router();
+var mkdirp = require("mkdirp");
+var fs = require("fs-extra");
+var resizeImg = require("resize-img");
+
+// Get Product model.
+var Product = require("../models/product");
+
+// Get pages index
+router.get("/", function(req, res) {
+  // mengambil data dari database
+  Page.find({})
+    .sort({ sorting: 1 })
+    .exec(function(err, pages) {
+      res.render("admin/pages", {
+        pages: pages
+      });
+    });
+});
+
+// Membuat add page dengan method GET
+router.get("/add-page", function(req, res) {
+  var title = "";
+  var slug = "";
+  var content = "";
+
+  res.render("admin/add_page", {
+    title: title,
+    slug: slug,
+    content: content
+  });
+});
+
+// Membuat method POST pada add page
+router.post("/add-page", function(req, res) {
+  req.checkBody("title", "Title must have a value").notEmpty();
+  req.checkBody("content", "Content must have a value").notEmpty();
+
+  var title = req.body.title;
+  var slug = req.body.slug.replace(/\s+/g, "-").toLowerCase();
+  if (slug == "") slug = title.replace(/\s+/g, "-").toLowerCase();
+
+  var content = req.body.content;
+
+  var errors = req.validationErrors();
+
+  if (errors) {
+    res.render("admin/add_page", {
+      errors: errors,
+      title: title,
+      slug: slug,
+      content: content
+    });
+  } else {
+    Page.findOne({ slug: slug }, function(err, page) {
+      if (page) {
+        req.flash("danger", "Page slug is exist, choose another slug");
+        res.render("admin/add_page", {
+          title: title,
+          slug: slug,
+          content: content
+        });
+      } else {
+        var page = new Page({
+          title: title,
+          slug: slug,
+          content: content,
+          sorting: 100
+        });
+
+        page.save(function(err) {
+          if (err) return console.log(err);
+
+          req.flash("success", "Page added!");
+          res.redirect("/admin/pages");
+        });
+      }
+    });
+  }
+});
+
+// Membuat POST sortable pages
+
+router.post("/reorder-pages", function(req, res) {
+  // console.log(req.body);
+
+  var ids = req.body["id[]"];
+
+  var count = 0;
+
+  for (var i = 0; i < ids.length; i++) {
+    var id = ids[i];
+    count++;
+
+    (function(count) {
+      Page.findById(id, function(err, page) {
+        page.sorting = count;
+        page.save(function(err) {
+          if (err) return console.log(err);
+        });
+      });
+    })(count);
+  }
+});
+
+// GET edit page
+
+router.get("/edit-page/:slug", function(req, res) {
+  Page.findOne({ slug: req.params.slug }, function(err, page) {
+    if (err) return console.log(err);
+
+    res.render("admin/edit_page", {
+      title: page.title,
+      slug: page.slug,
+      content: page.content,
+      id: page._id
+    });
+  });
+});
+
+// Membuat method POST pada edit page
+router.post("/edit-page/:slug", function(req, res) {
+  req.checkBody("title", "Title must have a value").notEmpty();
+  req.checkBody("content", "Content must have a value").notEmpty();
+
+  var title = req.body.title;
+  var slug = req.body.slug.replace(/\s+/g, "-").toLowerCase();
+  if (slug == "") slug = title.replace(/\s+/g, "-").toLowerCase();
+
+  var content = req.body.content;
+  var id = req.body.id;
+
+  var errors = req.validationErrors();
+
+  if (errors) {
+    res.render("admin/edit_page", {
+      errors: errors,
+      title: title,
+      slug: slug,
+      content: content,
+      id: id
+    });
+  } else {
+    Page.findOne({ slug: slug, _id: { $ne: id } }, function(err, page) {
+      if (page) {
+        req.flash("danger", "Page slug is exist, choose another slug");
+        res.render("admin/edit_page", {
+          title: title,
+          slug: slug,
+          content: content,
+          id: id
+        });
+      } else {
+        Page.findById(id, function(err, page) {
+          if (err) return console.log(err);
+
+          page.title = title;
+          page.slug = slug;
+          page.content = content;
+
+          page.save(function(err) {
+            if (err) return console.log(err);
+
+            req.flash("success", "Page has been update!");
+            res.redirect("/admin/pages/edit-page/" + page.slug);
+          });
+        });
+      }
+    });
+  }
+});
+
+// GET delete page
+router.get("/delete-pages/:id", function(req, res) {
+  Page.findByIdAndRemove(req.params.id, function(err) {
+    if (err) return console.log(err);
+
+    req.flash("success", "Page deleted!");
+    res.redirect("/admin/pages/");
+  });
+});
+
+// export
+module.exports = router;
